525f987a5cc2fbcb5f698b37b4986553ff9360bf
LuiscVillarrealL <luiscvillarreall@gmail.com>
2024-12-18 22:02:16 +0000

    kafka setup consumer

---
+++ src/main/java/com/lcvl/microservices/order/event/OrderPlacedEvent.java
@@ -0,0 +1,16 @@
+package com.lcvl.microservices.order.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+	
+	private String orderNumber;
+	private String email;
+	
+
+}
